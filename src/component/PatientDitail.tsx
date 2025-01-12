f031d21499b09fcce37771295ba460c4a9c266df
victor Alomia <alomia18@gmail.com>
2025-01-12 00:19:22 -0500

    incorporando funciones para la edicion y eliminacion de un paciente

--- src/component/PatientDitail.tsx
+++ src/component/PatientDitail.tsx
@@ -1,10 +1,12 @@
 import { type PacienteProps } from "../types/type";
 import { PatientDitalItem } from "./PatientDitalItem";
-
+import { usePatiens } from "../storage";
 type PatienDetailProps = {
   patient: PacienteProps;
 };
 export const PatientDitail = (patient: PatienDetailProps) => {
+  const detetePatient= usePatiens ((state)=>state.deletePatient)
+  const getPatientById =usePatiens((state)=>state.getPatientById)
   return (
     <div className="mx-5 my-10 px-5 py-10 shadow-md rounded-xl bg-white">
       <PatientDitalItem label="ID" data={patient.patient.id} />
@@ -12,7 +14,25 @@ export const PatientDitail = (patient: PatienDetailProps) => {
       <PatientDitalItem label="Propietario" data={patient.patient.caretaker} />
       <PatientDitalItem label="Email" data={patient.patient.email} />
       <PatientDitalItem label="Sintomas" data={patient.patient.symptoms} />
-      <PatientDitalItem label="Fecha de ingreso" data={String(patient.patient.date)} />
+      <PatientDitalItem
+        label="Fecha de ingreso"
+        data={String(patient.patient.date)}
+      />
+
+      <div className="flex justify-between mt-10 gap-3">
+        <button
+        onClick={()=>getPatientById(patient.patient.id)}
+        type="button" 
+        className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white font-bold uppercase">
+          Editar
+        </button>
+        <button 
+         type="button"
+         onClick={()=>detetePatient(patient.patient.id)}
+        className="py-2 px-10 bg-red-600 hover:bg-red-700 rounded-lg text-white font-bold uppercase">
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 };
--- src/storage.ts
+++ src/storage.ts
@@ -1,27 +1,45 @@
 import { create } from "zustand";
 import { DrafPatient, PacienteProps } from "./types/type";
-import {v4 as uuidv4}from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
-//Definiendo type para paciente
-type Patients ={
-    patients:PacienteProps[],
-    addpatients:(data:DrafPatient)=> void //Definiendo funciones que modifican el estado global
-}
+
+//Definiendo type para paciente y registrando acciones
+type Patients = {
+    activeId:PacienteProps["id"]
+  patients: PacienteProps[];
+  addpatients: (data: DrafPatient) => void; //Definiendo funciones que modifican el estado global
+  deletePatient:(id:PacienteProps['id'])=>void
+  getPatientById:(id:PacienteProps["id"])=>void
+};
+
 
 //Crea un paciente con un id incorporado
- const createPatient =(patients:DrafPatient):PacienteProps=>{
-  return{...patients,id:uuidv4()}
-
- }
+const createPatient = (patients: DrafPatient): PacienteProps => {
+  return { ...patients, id: uuidv4() };
+};
 
 //Creando el stado global con zustan
-export const usePatiens = create<Patients>((set)=>({
-    patients:[],
-    //Añadiendo la funcion al estado para ser utilizada
-    addpatients(data) {
-        const newPatient = createPatient(data)
-        set((state)=>({
-           patients: [...state.patients,newPatient]
-        }))
-    },
-}))
+export const usePatiens = create<Patients>((set) => ({
+  patients: [],
+  activeId:"",
+  //Añadiendo la funcion al estado para ser utilizada
+  addpatients:(data)=> {
+    
+    const newPatient = createPatient(data);
+    set((state) => ({
+      patients: [...state.patients, newPatient],
+    }));
+  },
+  //Elimina un paciente por su id
+  deletePatient:(id) =>{
+    set((state)=>({
+        patients:state.patients.filter(patients=>(patients.id !==id))
+    }))
+  },
+  getPatientById:(id)=>{
+     set(()=>({
+         activeId:id,
+     }))
+
+  }
+}));
